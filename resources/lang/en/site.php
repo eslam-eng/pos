2e7eed402457cd571437efa73b5526b36bdab4ec
Your Name <you@example.com>
2021-08-02 05:52:53 +0200

    init commit

---
+++ resources/lang/en/site.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'logout' => 'Logout',
+
+    'add' => 'add',
+    'create' => 'add',
+    'read' => 'show',
+    'edit' => 'edit',
+    'update' => 'update',
+    'delete' => 'delete',
+    'search' => 'search',
+    'show' => 'show',
+    'loading' => 'loading..',
+    'print' => 'print',
+
+    'confirm_delete' => 'are you sure?',
+    'yes' => 'yes',
+    'no' => 'no',
+
+    'login' => 'Login',
+    'remember_me' => 'remember me',
+    'password' => 'password',
+    'password_confirmation' => 'password confirmation',
+
+    'added_successfully' => 'Added successfully',
+    'updated_successfully' => 'updated successfully',
+    'deleted_successfully' => 'deleted successfully',
+
+    'no_data_found' => 'No data to show',
+    'no_records' => 'Unfortunately, there are no records',
+
+    'clients' => 'clients',
+    'client_name' => 'client name',
+    'phone' => 'phone',
+    'address' => 'address',
+    'previous_orders' => 'previous orders',
+    'orders' => 'orders',
+    'add_order' => 'add order',
+    'edit_order' => 'edit order',
+
+    'users' => 'Supervisors',
+    'first_name' => 'first name',
+    'last_name' => 'last name',
+    'email' => 'email',
+    'image' => 'image',
+    'action' => 'action',
+
+    'permissions' => 'permissions',
+
+    'categories' => 'categories',
+    'all_categories' => 'all categories',
+    'name' => 'name',
+    'description' => 'description',
+    'products_count' => 'products count',
+    'related_products' => 'related products',
+    'category' => 'category',
+    'show_products' => 'show products',
+    'created_at' => 'created_at',
+
+    'products' => 'products',
+    'product' => 'product',
+    'quantity' => 'quantity',
+    'total' => 'total',
+    'purchase_price' => 'purchase price',
+    'price' => 'price',
+    'sale_price' => 'sale_price',
+    'stock' => 'stock',
+    'profit_percent' => 'profit percent',
+
+    'ar' => [
+        'name' => 'name arabic',
+        'description' => 'arabic description',
+    ],
+
+    'en' => [
+        'name' => 'name english',
+        'description' => 'english description',
+    ],
+
+];
